a9c82a4d9e7c57730a3000d80de4551dd0c75a8c
Ferenc Kondasz <ferenc@getbynder.com>
2016-08-12 20:12:15 +0200

    Some changes

--- b-gitter.sh
+++ b-gitter.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 SELFDIR="$(dirname "$0")"
+
+source "${SELFDIR}"/b-gitter-options.sh
 REPOS=`find $ROOTDIR -type d -name '.git' -print | sed 's/.git//g'`
 
 if [[ -n "${1/[ ]*\n/}" ]]; then
 
     SEARCHBRANCH="$1"
-
-    source "${SELFDIR}"/b-gitter-options.sh
     source "${SELFDIR}"/b-gitter-checkout.sh
 else
     source "${SELFDIR}"/b-gitter-create.sh
